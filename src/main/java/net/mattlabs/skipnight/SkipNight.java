a2b4e1a6dc8b59afdafbf7b2f7c8fd8acef5dad2
mattboy9921 <mattboy9921@aim.com>
2017-10-18 19:33:21 -0400

    Optimize imports

--- src/main/java/net/mattlabs/skipnight/SkipNight.java
+++ src/main/java/net/mattlabs/skipnight/SkipNight.java
@@ -6,8 +6,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Level;
-
 public class SkipNight extends JavaPlugin {
 
     private Vote vote;
