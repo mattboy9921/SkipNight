d6348c3b5c5218f3c94c28bfa1fc53a2d538b1f1
mattboy9921 <mattboy9921@aim.com>
2018-11-28 17:47:08 -0500

    Implement ACF for commands

--- src/main/java/net/mattlabs/skipnight/SkipNight.java
+++ src/main/java/net/mattlabs/skipnight/SkipNight.java
@@ -1,30 +1,21 @@
 package net.mattlabs.skipnight;
 
+import co.aikar.commands.PaperCommandManager;
 import net.mattlabs.skipnight.commands.SkipNightCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SkipNight extends JavaPlugin {
 
-    private static SkipNight instance;
-    private SkipNightCommand skipNightCommand;
-    private Vote vote;
+    public Vote vote;
+    public PaperCommandManager manager;
 
     public void onEnable() {
-        instance = this;
         vote = new Vote(this);
 
-        // Register Command
-        skipNightCommand = new SkipNightCommand();
-        getCommand("skipnight").setExecutor(skipNightCommand);
+        // Register Commands with ACF
+        manager = new PaperCommandManager(this);
+        manager.registerCommand(new SkipNightCommand(this));
 
-        getLogger().info("SkipNight loaded - By mattboy9921 (Special thanks to RoyCurtis, iamliammckimm, CRX VrynzX, and Scarsz)");
-    }
-
-    public static SkipNight getInstance() {
-        return instance;
-    }
-
-    public Vote getVote() {
-        return vote;
+        getLogger().info("SkipNight loaded - By mattboy9921 (Special thanks to RoyCurtis, iamliammckimm, CRX VrynzX, Scarsz and Aikar)");
     }
 }
--- src/main/java/net/mattlabs/skipnight/commands/SkipNightCommand.java
+++ src/main/java/net/mattlabs/skipnight/commands/SkipNightCommand.java
@@ -1,34 +1,39 @@
 package net.mattlabs.skipnight.commands;
 
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.*;
+import com.google.common.collect.Lists;
 import net.mattlabs.skipnight.SkipNight;
 import net.mattlabs.skipnight.Vote;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import static org.bukkit.Bukkit.getLogger;
 
-public class SkipNightCommand implements CommandExecutor {
+@CommandAlias("skipnight|sn")
+public class SkipNightCommand extends BaseCommand {
 
-    Vote vote = SkipNight.getInstance().getVote();
+    Vote vote;
 
-    @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (!(commandSender instanceof Player)) {
+    public SkipNightCommand(SkipNight plugin) {
+        vote = plugin.vote;
+        plugin.manager.getCommandCompletions().registerStaticCompletion("skipnightOptions",
+                Lists.newArrayList("yes", "no"));
+    }
+
+    @Default
+    @Description("Starts a vote to skip the night.")
+    @CommandPermission("skipnight.vote")
+    @CommandCompletion("@skipnightOptions")
+    public void onDefault(CommandSender sender, @Optional@Values("@skipnightOptions") String vote) {
+        if (!(sender instanceof Player)) {
             getLogger().info("Vote can't be started from console.");
-            return true;
         }
-        Player player = (Player) commandSender;
-
-        if (strings.length == 0) {
-            vote.start(player);
+        else {
+            Player player = (Player) sender;
+            if (vote == null) this.vote.start(player);
+            else if (vote.equalsIgnoreCase("yes")) this.vote.addYes(player.getUniqueId());
+            else if (vote.equalsIgnoreCase("no")) this.vote.addNo(player.getUniqueId());
         }
-        else if (strings[0].equalsIgnoreCase("yes")) vote.addYes(player.getUniqueId());
-        else if (strings[0].equalsIgnoreCase("no")) vote.addNo(player.getUniqueId());
-        else player.sendMessage(ChatColor.RED + "Invalid usage: /skipnight [yes/no]");
-
-        return true;
     }
 }
