a4dee19635577e036653bd877053d4b203abe8ed
mattboy9921 <mattboy9921@aim.com>
2019-07-27 19:35:27 -0400

    Add day skip function

--- src/main/java/net/mattlabs/skipnight/util/FastForward.java
+++ src/main/java/net/mattlabs/skipnight/util/FastForward.java
@@ -7,15 +7,18 @@ public class FastForward implements Runnable {
 
     private World world;
     private Plugin plugin;
+    private VoteType voteType;
 
-    public FastForward(World world, Plugin plugin) {
+    public FastForward(World world, Plugin plugin, VoteType voteType) {
         this.world = world;
         this.plugin = plugin;
+        this.voteType = voteType;
     }
 
     @Override
     public void run() {
         world.setTime(world.getTime() + 80);
-        if (world.getTime() < 23900) plugin.getServer().getScheduler().runTaskLater(plugin, this, 1);
+        if (voteType == VoteType.NIGHT && world.getTime() < 23900) plugin.getServer().getScheduler().runTaskLater(plugin, this, 1);
+        if (voteType == VoteType.DAY && world.getTime() < 13900) plugin.getServer().getScheduler().runTaskLater(plugin, this, 1);
     }
 }
